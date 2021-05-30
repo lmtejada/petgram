bc1bbf0c6f4d03eb0d0c453e14c4b4f70474e727
Laura Tejada <laura_mtc@hotmail.com>
2021-05-30 16:57:52 -0500

    added category list

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { CategoryList } from './components/CategoryList'
+import { GlobalStyles } from './GlobalStyles'
+
+export const App = () => (
+  <div>
+    <GlobalStyles />
+    <CategoryList />
+  </div>
+)
---
+++ src/components/CategoryList/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Category } from '../Category'
+import { List, Item } from './styles'
+
+import { categories } from '../../../api/db.json'
+
+export const CategoryList = () => (
+  <List>
+    {
+      categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
+    }
+  </List>
+)

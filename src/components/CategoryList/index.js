441b67fc310fc3e9507c8998ba2cac2b0e8b8e2d
Laura Tejada <laura_mtc@hotmail.com>
2021-06-03 21:43:23 -0500

    added images lazy load

--- src/components/CategoryList/index.js
+++ src/components/CategoryList/index.js
@@ -1,13 +1,54 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Category } from '../Category'
 import { List, Item } from './styles'
 
-import { categories } from '../../../api/db.json'
+const useCategoriesData = () => {
+  const [categories, setCategories] = useState([])
+  const [loading, setLoading] = useState(false)
 
-export const CategoryList = () => (
-  <List>
-    {
-      categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
+  useEffect(() => {
+    setLoading(true)
+    window.fetch('https://petgram-server-lmtejada.vercel.app/categories')
+      .then(res => res.json())
+      .then(response => {
+        setCategories(response)
+        setLoading(false)
+      })
+  }, [])
+
+  return { categories, loading }
+}
+
+export const CategoryList = () => {
+  const { categories, loading } = useCategoriesData()
+  const [showFixed, setShowFixed] = useState(false)
+  const renderList = (fixed) => (
+    <List fixed={fixed}>
+      {
+        loading
+          ? <Item><Category /></Item>
+          : categories.map(
+            category => <Item key={category.id}><Category {...category} /></Item>
+          )
+      }
+    </List>
+  )
+
+  useEffect(() => {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
     }
-  </List>
-)
+
+    document.addEventListener('scroll', onScroll)
+
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
+  )
+}

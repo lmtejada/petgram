a73c5a72e96e3d52c238a95487c1c0e4317bd161
Laura Tejada <laura_mtc@hotmail.com>
2021-06-12 19:32:18 -0500

    using render props

--- src/App.js
+++ src/App.js
@@ -4,12 +4,26 @@ import { GlobalStyles } from './styles/GlobalStyles'
 import { CategoryList } from './components/CategoryList'
 import { Logo } from './components/Logo'
 import { PhotoCardList } from './container/PhotoCardList'
+import { PhotoCardWithQuery } from './container/PhotoCardWithQuery'
 
-export const App = () => (
-  <div>
-    <GlobalStyles />
-    <Logo />
-    <CategoryList />
-    <PhotoCardList categoryId={1} />
-  </div>
-)
+export const App = () => {
+  const urlParams = new window.URLSearchParams(window.location.search)
+  const detailId = urlParams.get('detail')
+
+  return (
+    <div>
+      <GlobalStyles />
+      <Logo />
+      {
+        detailId
+          ? <PhotoCardWithQuery id={detailId} />
+          : (
+            <>
+              <CategoryList />
+              <PhotoCardList categoryId={1} />
+            </>
+            )
+      }
+    </div>
+  )
+}

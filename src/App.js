b68d9d4f903807a18c184f1ef479c897c4818c1b
Laura Tejada <laura_mtc@hotmail.com>
2021-06-27 14:48:29 -0500

    added protected routes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,15 @@ import { GlobalStyles } from './styles/GlobalStyles'
 import { Logo } from './components/Logo'
 import { NavBar } from './components/NavBar'
 import { Home } from './pages/Home'
+
 import { Detail } from './pages/Detail'
+import { Favs } from './pages/Favs'
+import { User } from './pages/User'
+import { NotRegisteredUser } from './pages/NotRegisteredUser'
+
+const UserLogged = ({ children }) => {
+  return children({ isAuth: false })
+}
 
 export const App = () => {
   return (
@@ -17,6 +25,25 @@ export const App = () => {
         <Home path='/pet/:categoryId' />
         <Detail path='/detail/:detailId' />
       </Router>
+
+      <UserLogged>
+        {
+          ({ isAuth }) =>
+            isAuth
+              ? (
+                <Router>
+                  <Favs path='/favs' />
+                  <User path='/user' />
+                </Router>
+                )
+              : (
+                <Router>
+                  <NotRegisteredUser path='/favs' />
+                  <NotRegisteredUser path='/user' />
+                </Router>
+                )
+        }
+      </UserLogged>
       <NavBar />
     </div>
   )
